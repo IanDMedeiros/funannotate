7db546ef8887c8feb753a2c761aa92051fa76374
IanDMedeiros <80306126+IanDMedeiros@users.noreply.github.com>
2024-10-06 23:24:30 -0400

    Update funannotate.py
    Migrate to importlib

--- funannotate/funannotate.py
+++ funannotate/funannotate.py
@@ -8,12 +8,12 @@ import os
 import importlib
 import subprocess
 import funannotate
-from pkg_resources import get_distribution
+import importlib.metadata
 
 global package_name
 global __version__
 package_name = 'funannotate'
-__version__ = get_distribution(package_name).version
+__version__ = importlib.metadata.version(package_name)
 
 default_help = """
 Usage:       {:} <command> <arguments>
